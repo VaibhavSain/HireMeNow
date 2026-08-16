5ffed4dc1bced02af32ff024640058d609f178f4
Vaibhav Sain <xyzx010101@gmail.com>
2026-08-16 22:32:39 +0530

    added fetch data feature

--- backend/index.js
+++ backend/index.js
@@ -13,13 +13,12 @@ app.use((req, res, next) => {
 })
 
 //connect to db
+
 mongoose.connect(process.env.MONGO_URL).then(() => {
   //Listening for requests
   app.listen(process.env.PORT || 4000, () => {
     console.log("Connect to db & listening on port", process.env.PORT);
   });
+}).catch((error) => {
+  console.log(error);
 })
-  .catch((error) => {
-    console.log(error);
-  })
-
--- frontend/src/components/Listing.jsx
+++ frontend/src/components/Listing.jsx
@@ -1,12 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./Card";
+import axios from "axios";
 
 function Listing() {
-  const [data, setData] = useState();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const response = await axios.get("http://localhost:4000/api/jobs");
+        setData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchJobs();
+  }, []);
 
   return (
-    <div className="bg-[#faf8ff] pb-20 h-screen overflow-scroll pt-4 absolute top-[13rem] left-0 gap-4 flex flex-col  w-full">
-      <Card />
+    <div className="bg-[#faf8ff] pb-20 h-screen overflow-scroll pt-4 absolute top-[13rem] left-0 gap-4 flex flex-col w-full">
+      {data.map((job) => (
+        <Card key={job._id} />
+      ))}
     </div>
   );
 }
